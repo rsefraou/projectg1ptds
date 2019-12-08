58524d9fb5dac3239d11a810759a8094a1b4c26c
rsefraou <rita.sefraouitahiri@unil.ch>
2019-12-08 15:11:34 +0100

    Add stopwords to wordcloud_reddit

--- R/plot_wordcloud_reddit.R
+++ R/plot_wordcloud_reddit.R
@@ -11,24 +11,39 @@ plot_wordcloud_reddit <- function(word, stopwords) {
     sort_by = "new",
     time_frame = "day"
   )
-  
-  stopwords_vec <-
-    c(stopwords::stopwords("en"), "don", "isn", "gt", "i", word)
-  
+    stopwords_vec <- c(
+      stopwords::stopwords("en"),
+      "don",
+      "isn",
+      "gt",
+      "i",
+      "re",
+      "removed",
+      "deleted",
+      "m",
+      "you re",
+      "we ll",
+      "ve",
+      "hasn",
+      "they re",
+      "id",
+      "tl dr",
+      word
+    )
   data.1 <-
     projectg1ptds::reddit_content(data[1:10, 5], wait_time = 2)
-  
+
   data.1["comment"] <- tibble::as_tibble(sapply(
     data.1["comment"],
     projectg1ptds::cleaning_text_function,
     stopwords = c(stopwords::stopwords("en"), word, stopwords_vec)
   ))
-  
+
   contenu_wordcloud <- data.1 %>%
     tibble::as_tibble() %>%
     tidytext::unnest_tokens(word, comment) %>%
     dplyr::filter(is.na(as.numeric(word)))
-  
+
   contenu_wordcloud %>%
     dplyr::count(word) %>%
     with(wordcloud::wordcloud(
