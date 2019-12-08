8ed2dd9777296163c392556130c474d5c15b7eb9
rsefraou <rita.sefraouitahiri@unil.ch>
2019-12-08 15:09:20 +0100

    Change cleaning_text_function.R

--- R/cleaning_text_function.R
+++ R/cleaning_text_function.R
@@ -4,35 +4,34 @@
 #' @param stopwords  used to specify the stopwords we want to take off the dataframe
 #' @return the scraped data cleaned
 #' @export
-cleaning_text_function <- function(x,stopwords =stopwords_vec ){
-  if(is.character(x)) {
-    #condition to ingore characters columns with only internet link 
-    if (sum(str_sub(x, 1, 10) == "http://www",na.rm = TRUE) < length(x)/2) {
-      #met les accents à la place du code html 
-      Encoding(x) <- 'latin1'
-      #enleve les accents et met des lettres simples
-      x <- stri_trans_general(x, 'Latin-ASCII')  
-      x <- unlist(lapply(x, function(x, stopwords) {
-        #sépare tout les mots 
-        x <- unlist(strsplit(x, " "))
-        #Enlève liens dans les commentaires 
-        x <- x[!grepl("\\S+www\\S+|\\S+https://\\S+", x)] 
-        #Enlèves codes ASCII et la ponctuation
-        x <- gsub("\n|[[:punct:]]|[\x01-\x09\x11-\x12\x14-\x1F\x7F]", " ", x)
-        #Enlève les lettres solitaires 
-        x <- gsub("(\\s{1}|^)[a-z]{1}(\\s{1}|$)","", x)
-        #enlève les mots dans la liste stopwords 
-        x <-paste(x[!x %in% stopwords], collapse = " ")
-        #mets tout en lower case 
-        x <- tolower(x)
-        return(x)
-      },
-      stopwords = stopwords))
-    } else {
-      x
-    }
+cleaning_text_function <- function(x, stopwords=stopwords_vec) {
+
+  if (is.character(x)) {
+    #Put accents instead of code html (only for french)
+    Encoding(x) <- 'latin1'
+    #take out accent
+    x <- stri_trans_general(x, 'latin-ascii')
+    x <- unlist(lapply(x, function(x, stopwords = stopwords_vec) {
+      #separate words
+      x <- unlist(strsplit(x, " "))
+      #take out internet links
+      x <- x[!grepl("\\S+www\\S+|\\S+https://\\S+|https://\\S+", x)]
+      #take out codes ASCII and ponctuation
+      x <-gsub("\n|[[:punct:]]|[\x01-\x09\x11-\x12\x14-\x1F\x7F]|gt"," ",x)
+      #take out simple alone numbers
+      x <-gsub("(^[0-9]{1}\\s|^[0-9]{1}$|\\s{1}[0-9]{1}$|\\s{1}[0-9]{1}\\s{1})"," ",x)
+      #take out space in the beginning and end of stringg
+      x <-gsub("(^[[:blank:]]+|[[:blank:]]+$)", "", x)
+      #lowercase
+      x <- tolower(x)
+      #take out alone letters
+      x <-gsub("(^[a-z]{1}\\s+|^[a-z]{1}$|\\s+[a-z]{1}$|\\s+[a-z]{1}\\s+)", "", x)
+      #take out words in stopwords list
+      x <-paste(x[!x %in% stopwords], collapse = " ")
+      return(x)
+    }))
   } else{
-    x 
+    stop("please enter a character vector")
   }
   return(x)
 }
