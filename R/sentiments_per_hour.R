187cd1d10149c67a1a24c74d5590b6e891cf546e
rsefraou <rita.sefraouitahiri@unil.ch>
2019-12-08 14:48:32 +0100

    Create a script for sentiments_per_hour() (#1)

---
+++ R/sentiments_per_hour.R
@@ -0,0 +1,42 @@
+#' @title Get sentiments per hour of the day
+#'
+#' @param b  a database extracted from reddit_content()
+#' @return  plot
+#' @export
+sentiments_per_hour<-function(b){
+
+  #import database of type reddit_content()
+  b1 <- b
+  #formatting
+
+  b1$date<-lubridate::ymd_hm(b1$date)
+  b1<-b %>% dplyr::mutate(year = lubridate::year(date),
+                          month = lubridate::month(date),
+                          day = lubridate::day(date),
+                          hour = lubridate::hour(date),
+                          minute =lubridate :: minute(date))
+  b1$hour <-as.factor(b1$hour)
+  b1$ID <- seq.int(nrow(b1))
+
+  #Unnest tokens for sentiment analysis
+  b3<-b1%>%
+    tibble::as_tibble() %>%
+    tidytext::unnest_tokens(word, comment) %>%
+    dplyr::filter(is.na(as.numeric(word)))
+
+  #Sentiment analysis
+  drv_comment<-b3%>%
+    dplyr::group_by(ID)%>%
+    dplyr::inner_join(tidytext::get_sentiments("afinn"), by="word")
+
+  #Make them either positive or negative for a more easy visualisation
+  drv_comment2<-drv_comment%>%
+    mutate(sentiment=ifelse(value<0, "negative", "positive"))
+
+  #plot
+  ggplot(drv_comment2, aes(x=hour, fill=sentiment))+
+    geom_bar(aes())+
+    theme_minimal()+
+    ggtitle("Sentiments per hour")
+
+}
