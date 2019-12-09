f9cbb7c9251d9ec06b4897e00282f163dac5b6fa
rsefraou <rita.sefraouitahiri@unil.ch>
2019-12-09 15:02:02 +0100

    Add stopword vec to reddit_content()

--- R/cleaning_text_function.R
+++ R/cleaning_text_function.R
@@ -5,7 +5,7 @@
 #' @return the scraped data cleaned
 #' @export
 cleaning_text_function <- function(x, stopwords=stopwords_vec) {
-
+  stopwords_vec <- c(stopwords::stopwords("en"), "don", "isn", "gt", "i")
   if (is.character(x)) {
     #Put accents instead of code html (only for french)
     Encoding(x) <- 'latin1'
