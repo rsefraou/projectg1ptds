1c7b4ad7eba4045f541e370ef6241610727e27bb
rsefraou <rita.sefraouitahiri@unil.ch>
2019-12-09 20:31:16 +0100

    Create a new function that takes into it all the other scrapping functions

---
+++ R/scrapping_function.R
@@ -0,0 +1,20 @@
+#' @title Scrap data from reddit to get a table
+#'
+#' @param search_terms  The word we look for
+#' @param subreddit the subreddit we search in
+#' @param sort_by the sort by new, top...
+#' @param time_frame the time frame taken into account
+#' @return the scraped data cleaned
+#' @export
+scrapping_function <- function(search_terms, subreddit, 
+                               sort_by , time_frame){
+  
+  exctracted_link <- reddit_urls_mod(search_terms, subreddit, 
+                                     sort_by , time_frame)
+  
+  exctracted_data <- reddit_content(exctracted_link[,5])
+  
+  exctracted_data[,13] <- cleaning_text_function(exctracted_data[,13])
+  
+  return(exctracted_data)
+}
