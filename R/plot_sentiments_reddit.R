f582f9d004695dd56b9ddcba76bc44cde2fadf4c
Barbusse <david.pitteloud@unil.ch>
2019-12-10 09:30:54 +0100

    Modify the function so that it is aligned with the shiny requierements in terms of data frame (#2)

--- R/plot_sentiments_reddit.R
+++ R/plot_sentiments_reddit.R
@@ -5,46 +5,10 @@
 #' @return sentiments plot
 #' @importFrom magrittr "%>%"
 #' @export
-plot_sentiments_reddit <- function(word, stopwords) {
-  data <- projectg1ptds::reddit_urls_mod(
-    search_terms = word,
-    subreddit = NA,
-    sort_by = "top",
-    time_frame = "week"
-  )
 
-  stopwords_vec <- c(
-    stopwords::stopwords("en"),
-    "don",
-    "isn",
-    "gt",
-    "i",
-    "re",
-    "removed",
-    "deleted",
-    "m",
-    "you re",
-    "we ll",
-    "ve",
-    "hasn",
-    "they re",
-    "id",
-    "tl dr",
-    word
-  )
+plot_sentiments_reddit <- function(df) {
 
-  data.1 <-
-    projectg1ptds::reddit_content(data[1:10, 5], wait_time = 2)
-
-  data.1["comment"] <- tibble::as_tibble(sapply(
-    data.1["comment"],
-    projectg1ptds::cleaning_text_function,
-    stopwords = c(stopwords::stopwords("en"),
-                  word,
-                  stopwords_vec)
-  ))
-
-  contenu_wordcloud <- data.1 %>%
+  contenu_wordcloud <- df %>%
     tibble::as_tibble() %>%
     tidytext::unnest_tokens(word, comment) %>%
     dplyr::filter(is.na(as.numeric(word)))
@@ -65,4 +29,3 @@ plot_sentiments_reddit <- function(word, stopwords) {
     ggplot2::labs(x = "", y = "Number of words", fill = "Sentiment")
 
 }
-
