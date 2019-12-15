97cc5685863621de4bc0ecd135148c5d382ca345
rsefraou <rita.sefraouitahiri@unil.ch>
2019-12-15 14:59:52 +0100

    Create runDemo function

---
+++ R/runDemo.R
@@ -0,0 +1,16 @@
+#' @title Run demonstration of Reddit Analyzer App
+#'
+#' @describe runDemo opens the app 
+#' @export
+runDemo <- function() {
+  appDir <- system.file("shiny-examples",  package = "projectg1ptds")
+  if (appDir == "") {
+    stop(
+      "Could not find example directory. Try re-installing projectg1ptds",
+      call. = FALSE
+    )
+  }
+  
+  shiny::runApp(appDir, display.mode = "normal")
+  
+}
